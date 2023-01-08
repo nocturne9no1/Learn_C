8977cc41dac95338733b3f69340772de16f255a3
nocturne9no1 <nocturne9no1@gmail.com>
2023-01-09 00:27:40 +0900

    feat: input list

--- CForAll/004_string_input/scanf.c
+++ CForAll/004_string_input/scanf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 int main() {
-  double celsius;
+  double celsius;  // 섭씨 온도
 
   printf("섭씨 온도를 화씨로 바꿔줍니다.\n");
   printf("섭시 온도 입력해바라: ");
-  scanf("%lf", &celsius);
+  scanf("%lf", &celsius);  // 입력 받기
 
   printf("섭씨%f 도는 화씨로 %f 도 입니다.\n", celsius, 9 * celsius / 5 + 32);
 
