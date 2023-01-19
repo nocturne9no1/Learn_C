debeca9b2d6c018488433045fcb0d89676f4c71b
nocturne9no1 <nocturne9no1@gmail.com>
2023-01-19 22:13:07 +0900

    feat: CForAll - 010 pointer * position with const

--- CForAll/010_pointer/05_constant_pointer.c
+++ CForAll/010_pointer/05_constant_pointer.c
@@ -3,7 +3,8 @@
 int main() {
   int a = 2;
   int b = 3;
-  const int *pa = &a;
+  const int *pa = &a;  // a 에 저장된 값이 바뀌면 안되는 것!
+  int* const pb = &b;  // pb 에 저장된 포인터 값(주소값)이 바뀌면 안되는 것!
 
   // *pa = 3;  // not correct - error: read-only variable is not assignable
   printf("%d\n", *pa);
